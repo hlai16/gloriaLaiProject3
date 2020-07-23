54870b836685e9a6cf1e9383035f37550546c606
Gloria <gloriajoanlai@gmail.com>
2020-07-23 01:56:18 -0400

    html done, content done, setup done :V:

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// I am so not good at this...
+
+// 1) Document Ready
+// 2) Add event listener to form submit
+// 3) Add e.preventDefault() to prevent page refresh
+// 4) listen to user checked radio buttons.
+// 5) retrieve and store user's choice of radio buttons in a const(s)
+// 6) post correct answers once user submits answers.
+
